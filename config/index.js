437e10f28ebc891932de4aec1b3738c318251118
pxlmystic <pxlmystic69@gmail.com>
2022-06-18 18:14:37 -0400

    remove readme for now

---
+++ config/index.js
@@ -0,0 +1,11 @@
+export const config = {
+  env: "local",
+  LOCAL_API_URL: "localhost:3000",
+  API_URL: "odp-rarity.vercel.app",
+  COLLECTION_NAME: "TinyDinosSummer2022",
+  COLLECTION_TITLE: "tiny dinos: summer 2022 (unofficial)",
+  COLLECTION_DESCRIPTION: "cc0 omnichain nft | summer 2022 event collection | #rawr",
+  COLLECTION_IMG_LINK: "https://openseauserdata.com/files/1dc25155335b0fc76868a53b52717959.png",
+  STARTING_INDEX: 0,
+  NODE_OPTIONS: '--openssl-legacy-provider'
+};
